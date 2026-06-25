b014d7d0e6d06ba405ccfea6f1fcc59497c8c026
Bop Hung <nookun987@gmail.com>
2026-06-25 20:29:42 +0700

    Remove cancelled film v04 outputs; path-neutral governance docs

--- production/character/build_log/GATHER_REEL_V0_1/RUN_FILM_V04.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Build MIKAGE BUILD LOG FILM_FULL_V0_4 (appends the V1.4 "IT LEARNS TO WALK" gait chapter).
-REM Needs: Python 3 + pillow + numpy, ffmpeg/ffprobe on PATH. Output stays PROTOTYPE / local only.
-cd /d "%~dp0"
-python build_film_v04.py
-echo.
-echo Done. Press any key to close.
-pause >nul
